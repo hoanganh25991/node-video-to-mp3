4efa99308415b115bce46daabd84bc1d251b4e25
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-06 15:52:47 +0700

    Convert stream store into output stream

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const audioMp3 = "mp3";
 const testMp4 = `${curDir}/test.mp4`;
 const testMp3 = `${curDir}/test.mp3`;
 
+const timestamp = new Date().getTime()
+const outStream = fs.createWriteStream(`${timestamp}.mp3`);
+
 const run1 = () => {
 	// Simple run on full video
 	ffmpeg(testMp4)
@@ -18,7 +21,7 @@ const run1 = () => {
 
 const run = () => {
 	const len = 1600000;
-	const ffStream = ffmpeg(testMp4).format(audioMp3).pipe();
+	const ffStream = ffmpeg(testMp4).format(audioMp3).on("end", () => outStream.end()).pipe();
 
 	let downloaded = 0;
 
@@ -26,6 +29,7 @@ const run = () => {
 	ffStream
 	.on("data", chunk => {
 		downloaded += chunk.length;
+		outStream.write(chunk)
 		process.stdout.write(`Sending ${(100.0 * downloaded / len).toFixed(2)}% ${downloaded} bytes \r`)
 	})
 }
