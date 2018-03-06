d84df61b09cecb305e43618af014929c957760c3
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-06 16:05:39 +0700

    Transform & send by stream

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const run1 = () => {
 	.run();
 }
 
-const run = () => {
+const run2 = () => {
 	const len = 1600000;
-	const ffStream = ffmpeg(testMp4).format(audioMp3).on("end", () => outStream.end()).pipe();
+	const ffStream = ffmpeg(testMp4).format(audioMp3).pipe();
 
 	let downloaded = 0;
 
@@ -32,8 +32,29 @@ const run = () => {
 		outStream.write(chunk)
 		process.stdout.write(`Sending ${(100.0 * downloaded / len).toFixed(2)}% ${downloaded} bytes \r`)
 	})
+	.on("finish", () => outStream.end())
 }
 
+const run = () => {
+	const server = http.createServer((req, res) => {
+		const len = 1600000;
+		const ffStream = ffmpeg(testMp4).format(audioMp3).pipe();
 
+		let downloaded = 0;
+
+		res.setHeader('Content-disposition', 'attachment; filename=' + "test.mp3");
+		res.setHeader('Content-type', 'application/octet-stream');
+
+		// outStream
+		ffStream.on("data", chunk => {
+			downloaded += chunk.length;
+			process.stdout.write(`Sending ${(100.0 * downloaded / len).toFixed(2)}% ${downloaded} bytes \r`)
+			res.write(chunk);
+		})
+		.on("finish", () => res.end())
+
+		// ffStream.on("end", () => res.end())
+	}).listen(3000);
+}
 
 run();
