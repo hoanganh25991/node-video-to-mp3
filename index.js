95d23b00dbdb12f148450e03c94377fbd49b5a34
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-06 15:51:09 +0700

    Convert as stream

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var ffmpeg = require('fluent-ffmpeg');
+const ffmpeg = require("fluent-ffmpeg");
+const fs = require("fs")
+const http = require('http');
 
 const curDir = __dirname;
 const audioMp3 = "mp3";
@@ -6,8 +8,28 @@ const audioMp3 = "mp3";
 const testMp4 = `${curDir}/test.mp4`;
 const testMp3 = `${curDir}/test.mp3`;
 
+const run1 = () => {
+	// Simple run on full video
+	ffmpeg(testMp4)
+	.format(audioMp3)
+	.output(testMp3)
+	.run();
+}
 
-ffmpeg(testMp4)
-.format(audioMp3)
-.output(testMp3)
-.run();
+const run = () => {
+	const len = 1600000;
+	const ffStream = ffmpeg(testMp4).format(audioMp3).pipe();
+
+	let downloaded = 0;
+
+	// outStream
+	ffStream
+	.on("data", chunk => {
+		downloaded += chunk.length;
+		process.stdout.write(`Sending ${(100.0 * downloaded / len).toFixed(2)}% ${downloaded} bytes \r`)
+	})
+}
+
+
+
+run();
