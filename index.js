b0a27f81df740f40d67cd36d10bd8f136202ae6e
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-07 10:38:00 +0700

    Still not find out WHY, google's url not stream data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs")
 const https = require("https");
 const http = require("http");
+const { URL } = require('url');
 
 const curDir = __dirname;
 const audioMp3 = "mp3";
@@ -39,34 +40,36 @@ const run2 = () => {
 }
 
 const convertNStream = (url, res) => {
-	const options = {
-		host: "tinker.press",
-		port: 443,
-		path: "/videos/def.mp4",
-		method: "GET",
-		headers: {
-			"content-type": "video/mp4"
-		}
-	};
+	const pUrl = new URL(url);
+	
+	// const options = {
+	// 	host: pUrl.host,
+	// 	port: 443,
+	// 	path: pUrl.pathname,
+	// 	method: "GET",
+	// 	headers: {
+	// 		"content-type": "audio/m4a"
+	// 	}
+	// };
 
-	https.get(options).on("response", (videoStream) => {
+	https.get(url).on("response", (videoStream) => {
 		const {headers, statusCode} = videoStream
 		_("[headers, statusCode]", headers, statusCode)
 		// videoStream.on("data", chunk => _(chunk))
-		// const ffStream = ffmpeg(videoStream).format("mp4").on("error", (err, stdout, stderr) => {
-  //               _("[ERR]", err.message, err, stderr);
-  //           }).pipe();
-		// const timestamp = new Date().getTime();
+		const ffStream = ffmpeg(videoStream).format(audioMp3).on("error", (err, stdout, stderr) => {
+                _("[ERR]", err.message, err, stderr);
+            }).pipe();
+		const timestamp = new Date().getTime();
 
-		// res.setHeader("Content-disposition", `attachment; filename=${timestamp}.mp3`);
-		// res.setHeader("Content-type", "application/octet-stream");
+		res.setHeader("Content-disposition", `attachment; filename=${timestamp}.mp3`);
+		res.setHeader("Content-type", "application/octet-stream");
 
-		// // outStream
-		// ffStream.on("data", chunk => {
-		// 	_("Chunk size", chunk.length)
-		// 	res.write(chunk);
-		// })
-		// .on("finish", () => res.end())
+		// outStream
+		ffStream.on("data", chunk => {
+			// _("Chunk size", chunk.length)
+			res.write(chunk);
+		})
+		.on("finish", () => res.end())
 		
 	})
 }
@@ -77,6 +80,7 @@ const run = () => {
 		// const url = "https://tinker.press/videos/say-hello-to-cozmo.mp4";
 		// const url = "https://tinker.press/videos/abc.mp4";
 		const url = "https://tinker.press/videos/def.mp4";
+		// const url = "https://r3---sn-oguesnzl.googlevideo.com/videoplayback?ipbits=0&dur=343.608&fvip=3&expire=1520408387&itag=140&clen=5458032&signature=47024B57FDD1D5EADA18CBC607D7CE9C43297D13.0198FFF7EAD7BFA80AA528F8278010B7125C02C1&mime=audio/mp4&gir=yes&key=yt6&keepalive=yes&c=WEB&ei=40KfWqb8EcaEqQHy7I_ACw&lmt=1509079416429548&mt=1520386672&source=youtube&mm=31,26&mn=sn-oguesnzl,sn-i3b7knlk&id=o-AEfvbw0_wCSVjb4eK4xfU_hN1CLjbHGlWkfRYk4VqJRX&pl=26&ip=45.76.211.250&beids=[9466594]&requiressl=yes&initcwndbps=555000&sparams=clen,dur,ei,gir,id,initcwndbps,ip,ipbits,itag,keepalive,lmt,mime,mm,mn,ms,mv,pl,requiressl,source,expire&mv=m&ms=au,onr&signature=47024B57FDD1D5EADA18CBC607D7CE9C43297D13.0198FFF7EAD7BFA80AA528F8278010B7125C02C1";
 		convertNStream(url, res)
 	}).listen(3000);
 }
