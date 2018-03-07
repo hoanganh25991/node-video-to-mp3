b2213b83ea8f6c57a18b67680bdae53dec898ea9
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-07 20:37:08 +0700

    Bit rate default as 320k

--- src/convert.js
+++ src/convert.js
@@ -4,9 +4,15 @@ const handleErr = err => {
   console.log("[convertStreamToMp3][ERR]", err)
 }
 
+const printCmd = cmd => {
+  console.log("Spawned Ffmpeg with command: " + cmd)
+}
+
 export const convertStreamToMp3 = videoStream => {
   return ffmpeg(videoStream)
-    .format("mp3")
+    .on("start", printCmd)
     .on("error", handleErr)
+    .format("mp3")
+    .outputOptions("-ab 320k")
     .pipe()
 }
