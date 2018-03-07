bd9185e548ef8e670c3326aaa1bf4c5c88cfa382
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-07 12:11:23 +0700

    Hex to str

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import http from "http"
 import { getVideoStream } from "./getVideoStream"
 import { convertStreamToMp3 } from "./convert"
 import { msg } from "./message"
-import url from "url"
+import URL from "url"
 
 const setDownloadHeader = res => {
   const timestamp = new Date().getTime()
@@ -10,23 +10,33 @@ const setDownloadHeader = res => {
   res.setHeader("Content-type", "application/octet-stream")
 }
 
+const hexToString = hex => {
+  let string = ""
+  for (let i = 0; i < hex.length; i += 2) {
+    string += String.fromCharCode(parseInt(hex.substr(i, 2), 16))
+  }
+  return string
+}
+
 const getUrl = req => {
-  const { query } = url.parse(req.url, true)
-  console.log(query)
+  const { query = {} } = URL.parse(req.url, true)
+  const { url: hexUrl } = query
+  const url = hexToString(hexUrl)
+  console.log("[hexUrl, url]", hexUrl, url)
+
+  return url
 }
 
 const run = () => {
   http
     .createServer(async (req, res) => {
-      setDownloadHeader(res)
       const url = getUrl(req)
-
       if (!url) return res.end(msg.GET_VIDEO_FAIL)
 
       const videoStream = await getVideoStream(url)
-
       if (!videoStream) return res.end(msg.GET_VIDEO_FAIL)
 
+      setDownloadHeader(res)
       const convertStream = convertStreamToMp3(videoStream)
       convertStream.on("data", chunk => res.write(chunk))
       convertStream.on("finish", () => res.end())
